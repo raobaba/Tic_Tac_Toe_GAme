fa6ff0f38d7b522ceadd88a67d3d9a58c626bfad
raobaba <shrikishunr7@gmail.com>
2023-03-29 18:54:52 +0530

    making board page

---
+++ src/Pages/Board.tsx
@@ -0,0 +1,13 @@
+
+
+const Board:React.FC = () =>{
+    return (
+        <div>
+          <p>Hey,X it's your turn</p>
+          <div>
+            BOARD
+          </div>
+        </div>
+    )
+}
+export default Board;
